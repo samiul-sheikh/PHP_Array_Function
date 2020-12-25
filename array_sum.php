2b2eea2ec069ad2133c2018946a6f41d7a2deffb
samiul-sheikh <sheikh.samiul1971@gmail.com>
2020-12-26 02:43:57 +0600

    sum function

---
+++ array_sum.php
@@ -0,0 +1,72 @@
+<!docttype html>
+<html>
+    <head>
+        <title> PHP Syntax </title>
+
+        <style>
+        	.phpcoding{width:900px; margin:0 auto; background:grey; min-height: 400px;}
+
+        	.headerportion, .footerportion{background:#444; color:white; text-align: center; padding: 20px;}
+
+        	.headerportion h2, .footerportion h2{margin:0;}
+
+        	.maincontent{min-height: 400px; padding: 20px;}
+
+             p{margin: 0}
+
+        </style>
+
+    </head>
+
+
+    <body>
+
+    	<div class ="phpcoding">
+
+
+    		<section class="headerportion">
+    			<h2> <?php echo "PHP Array_Function" ?> </h2>
+    		</section>
+
+
+    		<section class="maincontent">
+                
+
+		    	<?php
+
+                    /*
+
+                    $number =array("10", "20", "30");
+
+                    $result =array_sum($number);
+
+                    echo "Total : ".$result;
+
+		    		*/
+
+
+                    $num =array("a" => "5", "b" => "10", "c" => "20");
+
+                    $result =array_sum($num);
+
+                    echo $result;
+
+                    
+
+    			?>
+
+    		</section>
+
+
+        	<section class="footerportion">
+                <p> &copy; <?php echo date("Y") ?> Samiul Sheikh </p>
+        		<h2> <?php echo "Presented By - Samiul Sheikh" ?> </h2>
+        	</section>
+
+
+    	</div>
+
+
+    </body>
+
+</html>
