21f6b2e4a81eb0cf2a53c80ac57cdc70a6c5edda
samiul-sheikh <sheikh.samiul1971@gmail.com>
2020-12-27 22:23:04 +0600

    variable & value generate in assosiative array

---
+++ array_compact.php
@@ -0,0 +1,66 @@
+<!docttype html>
+<html>
+    <head>
+        <title> PHP Syntax </title>
+
+        <style>
+        	.phpcoding{width:900px; margin:0 auto; background:grey; min-height: 400px;}
+
+        	.headerportion, .footerportion{background:#444; color:white; text-align: center; padding: 20px;}
+
+        	.headerportion h2, .footerportion h2{margin:0;}
+
+        	.maincontent{min-height: 400px; padding: 20px;}
+
+             p{margin: 0}
+
+        </style>
+
+    </head>
+
+
+    <body>
+
+    	<div class ="phpcoding">
+
+
+    		<section class="headerportion">
+    			<h2> <?php echo "PHP Array_Function" ?> </h2>
+    		</section>
+
+
+    		<section class="maincontent">
+                
+
+		    	<?php
+
+
+                    $name  = "Samiul";
+                    $dept  = "software engineering";
+                    $coder = "php";
+
+
+                    $result = compact("name", "dept", "coder");
+		    		
+
+                    print("<pre>");
+                    print_r($result);
+                    print("</pre>");
+
+    			?>
+
+    		</section>
+
+
+        	<section class="footerportion">
+                <p> &copy; <?php echo date("Y") ?> Samiul Sheikh </p>
+        		<h2> <?php echo "Presented By - Samiul Sheikh" ?> </h2>
+        	</section>
+
+
+    	</div>
+
+
+    </body>
+
+</html>
